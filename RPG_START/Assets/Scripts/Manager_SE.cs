f1d9961e8bc5ecd560ea42b61b649ccb2bbae6e9
ksc0116 <oeodo0116@naver.com>
2022-06-01 23:44:31 +0900

    SoundManager

---
+++ RPG_START/Assets/Scripts/Manager_SE.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager_SE : MonoBehaviour
+{
+    public AudioSource seAudio;
+
+    [Header("[Clip]")]
+    public AudioClip step;
+}
