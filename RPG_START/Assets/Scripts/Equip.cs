d1744ed56f88e3fd89a800f0febb5759aac3f0ef
ksc0116 <oeodo0116@naver.com>
2022-06-02 21:06:48 +0900

    Increase Stats By Equipment

--- RPG_START/Assets/Scripts/Equip.cs
+++ RPG_START/Assets/Scripts/Equip.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Equip : MonoBehaviour
 {
     [Header("[Player]")]
+    /*¡Ü*/public PlayerState player;
     public Mesh[] originMesh;
     public SkinnedMeshRenderer[] parts_Player;
 
@@ -21,6 +22,8 @@ public class Equip : MonoBehaviour
             cur_Equip[item.equipNum].equipped = false;
             cur_Equip[item.equipNum].transform.GetChild(0).gameObject.SetActive(false);
             Destroy(slot_Equip[item.equipNum].GetChild(1).gameObject);
+
+            /*¡Ü*/ReduceStats(cur_Equip[item.equipNum]);
         }
 
         GameObject item_Slot = Instantiate(item.gameObject, slot_Equip[item.equipNum]);
@@ -30,6 +33,8 @@ public class Equip : MonoBehaviour
         item.equipped = true;
         item.transform.GetChild(0).gameObject.SetActive(true);
         parts_Player[item.equipNum].sharedMesh = item.mesh;
+
+        /*¡Ü*/IncreaseStats(cur_Equip[item.equipNum]);
 
         Manager.instance.manager_Inven.itemInfoFrame.SetActive(false);
         Manager.instance.manager_Inven.itemInfoFrame.SetActive(true);
@@ -43,10 +48,37 @@ public class Equip : MonoBehaviour
 
         item.equipped = false;
         item.transform.GetChild(0).gameObject.SetActive(false);
+
+        /*¡Ü*/ReduceStats(cur_Equip[item.equipNum]);
+
         cur_Equip[item.equipNum] = null;
         parts_Player[item.equipNum].sharedMesh = originMesh[item.equipNum];
 
         Manager.instance.manager_Inven.itemInfoFrame.SetActive(false);
         Manager.instance.manager_Inven.itemInfoFrame.SetActive(true);
     }
+
+   /*¡Ü*/void IncreaseStats(Items_Info item)
+   /*¡Ü*/{
+   /*¡Ü*/    player.hp += item.hpBonus;
+   /*¡Ü*/    player.hp_Cur+=item.hpBonus;
+   /*¡Ü*/    player.atk+=item.atkBonus;
+   /*¡Ü*/    player.def+=item.defBonus;
+   /*¡Ü*/    player.cri+=item.criBonus;
+   /*¡Ü*/    
+   /*¡Ü*/     Manager.instance.manager_Inven.charInfoFrame.SetActive(false);
+   /*¡Ü*/     Manager.instance.manager_Inven.charInfoFrame.SetActive(true);
+   /*¡Ü*/}
+    
+    /*¡Ü*/void ReduceStats(Items_Info item)
+    /*¡Ü*/{
+    /*¡Ü*/    player.hp -= item.hpBonus;
+    /*¡Ü*/    player.hp_Cur -= item.hpBonus;
+    /*¡Ü*/    player.atk -= item.atkBonus;
+    /*¡Ü*/    player.def -= item.defBonus;
+    /*¡Ü*/    player.cri -= item.criBonus;
+    /*¡Ü*/
+    /*¡Ü*/    Manager.instance.manager_Inven.charInfoFrame.SetActive(false);
+    /*¡Ü*/    Manager.instance.manager_Inven.charInfoFrame.SetActive(true);
+    /*¡Ü*/}
 }
